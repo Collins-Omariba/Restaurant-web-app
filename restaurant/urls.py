8e49331cdf8f1aab2a78c6439b74e9a3835e03ab
Collins Omariba <collinsomariba@gmail.com>
2023-01-20 11:16:28 +0300

    Add and configured table booking api

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,9 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
+
+router = DefaultRouter()
+router.register(r'tables', views.BookingViewSet)
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('menu', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('booking', include(router.urls))
+                                                           
 ]
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Booking, Menu
@@ -10,22 +10,6 @@ from  .serializers import BookingSerializer, MenuSerializer
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
-
-
-
-class BookingView(APIView):
-
-    def get(self, request):
-        items = Booking.objects.all()
-        serializer = BookingSerializer
-        return Response(serializer.data)
-    
-    def post(self, request):
-        serializer = BookingSerializer
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"status": "success", "data": serializer.data})
 
 
 class MenuItemView(generics.ListCreateAPIView):
@@ -50,3 +34,8 @@ class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
 		if self.request.method != 'GET':
 			permission_classes = [IsAuthenticated]
 		return [permission() for permission in permission_classes]
+
+class BookingViewSet(viewsets.ModelViewSet):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
+	permission_classes = [IsAuthenticated]
