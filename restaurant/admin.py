eb994e15c2ee8d988b154b1164e68f4cd507a35d
Collins Omariba <collinsomariba@gmail.com>
2023-01-20 10:21:15 +0300

    Add Models

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Menu, Booking
 # Register your models here.
+
+admin.site.register(Menu)
+admin.site.register(Booking)
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.SmallIntegerField()
+    booking_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, db_index=True)
+    Inventory = models.SmallIntegerField()
+
+    def __str__(self):
+        return self.title
+    
