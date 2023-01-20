28140d6a8d1de3d4224579fbbae0b77b2dfea859
Collins Omariba <collinsomariba@gmail.com>
2023-01-20 08:34:59 +0300

    Removed reservation app and setup restaurant app.
    Added templates.

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "reservation",
+    "restaurant",
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "littlelemon.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ['templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- restaurant/apps.py
+++ restaurant/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ReservationConfig(AppConfig):
+class RestaurantConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "reservation"
+    name = "restaurant"
