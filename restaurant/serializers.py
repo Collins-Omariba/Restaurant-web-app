90360fceebbeddc94068fbb70aa269950e5bbe5c
Collins Omariba <collinsomariba@gmail.com>
2023-01-20 10:39:40 +0300

    Add serializers

---
+++ restaurant/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Menu, Booking
+
+class MenuSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = '__all__'
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
