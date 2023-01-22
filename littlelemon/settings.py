f7c5bf685c2c064c873289fd6b8ac3e79d84e264
Collins Omariba <collinsomariba@gmail.com>
2023-01-22 18:51:41 +0300

    Integrate djoser token authentication

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "restaurant",
     "rest_framework",
+    "rest_framework.authtoken",
+    "djoser",
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,22 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+REST_FRAMEWORK = {
+    
+    'DEFAULT_AUTHENTICATION_CLASSES':[
+        'rest_framework.authentication.TokenAuthentication',
+        "rest_framework.authentication.SessionAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
+}
+
+
+DJOSER={
+    "USER_ID_FIELD":"username"
+}
--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("restaurant/", include('restaurant.urls'))
+    path("restaurant/", include('restaurant.urls')),
+    path("auth/", include('djoser.urls')),
+    path("auth/", include('djoser.urls.authtoken'))
 ]
