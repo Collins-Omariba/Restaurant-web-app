299055b63096c87e2d1e50d767b41097b5d16edb
Collins Omariba <collinsomariba@gmail.com>
2023-01-23 16:52:34 +0300

    Auth token view url path

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 from . import views
 
@@ -10,7 +11,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu', views.MenuItemView.as_view()),
     path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
-    path('booking', include(router.urls))
+    path('booking', include(router.urls)),
+    path('api-token-auth', obtain_auth_token)
                                                            
 ]
 
