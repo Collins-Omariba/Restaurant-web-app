8840a80dc6c1913c3f02e5deb7c1d3ef61949f05
Collins Omariba <collinsomariba@gmail.com>
2023-02-04 17:21:19 +0300

    Registered BoookingTemplate & MenuTemplate models to the admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Menu, Booking
+from .models import Menu, Booking, BookingTemplate, MenuTemplate
 # Register your models here.
 
 admin.site.register(Menu)
 admin.site.register(Booking)
+admin.site.register(MenuTemplate)
+admin.site.register(BookingTemplate)
